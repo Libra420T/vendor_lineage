357a9f2acd758f0558fdbfb17ceba234261a3f97
Libra420T <ryantf420@gmail.com>
2024-12-28 19:17:57 -0500

    Fixup!: Vendor: Drop our custom soong header generator Enough significant changes have been made to soong in Android 12 for us to continue using this without a potential rewired logic. On top of that, we've decided internally to use device_kernel_headers and qti_kernel_headers as they are gonna replace our generated_kernel_headers. Good bye this goes.

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,33 +1,6 @@
-PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
-
-# Add variables that we wish to make available to soong here.
-EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
-    KERNEL_BUILD_OUT_PREFIX \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_CMD \
-    KERNEL_MAKE_FLAGS \
-    PATH_OVERRIDE_SOONG \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
-
-# Setup SOONG_CONFIG_* vars to export the vars listed above.
-# Documentation here:
-# https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
-
-SOONG_CONFIG_NAMESPACES += lineageVarsPlugin
-
-SOONG_CONFIG_lineageVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_lineageVarsPlugin += $(1)
-  SOONG_CONFIG_lineageVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
-
 SOONG_CONFIG_NAMESPACES += lineageGlobalVars
 SOONG_CONFIG_lineageGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_override_format_from_reserved \
