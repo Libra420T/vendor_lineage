c9f246428caf44dc58c883c86c8c893f1b1dff97
Libra420T <ryantf420@gmail.com>
2024-12-28 19:28:47 -0500

    Config: Include Native Freeform

--- config/common.mk
+++ config/common.mk
@@ -268,4 +268,8 @@ include vendor/lineage/config/version.mk
 # Partition overlay
 PRODUCT_COPY_FILES += \
     vendor/lineage/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
+    
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
