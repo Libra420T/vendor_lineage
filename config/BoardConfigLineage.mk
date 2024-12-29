c47bda2d83fe851c940093650dde100a113d06bc
Libra420T <ryantf420@gmail.com>
2024-12-28 19:15:56 -0500

    Vendor: remove kernel build task We will move fully to an AOSP build system with prebuilt kernel
    Change-Id: I19a0ab0be1154780aa8b0bf46cdc1272a1efd7e1
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/8176
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -4,8 +4,6 @@
 # Recovery
 BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 
-include vendor/lineage/config/BoardConfigKernel.mk
-
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
